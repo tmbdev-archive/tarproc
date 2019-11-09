9630cba80ee4964adaedce1bb95909b34fe05c83
Tom <tmbdev@gmail.com>
2019-11-09 14:27:41 -0800

    missing file

---
+++ tarproclib/proc.py
@@ -0,0 +1,33 @@
+import random
+
+def ishuffle(data, bufsize=1000, initial=100):
+    """Shuffle the data in the stream.
+
+    This uses a buffer of size `bufsize`. Shuffling at
+    startup is less random; this is traded off against
+    yielding samples quickly.
+
+    :param data: iterator
+    :param bufsize: buffer size for shuffling
+    :returns: iterator
+
+    """
+    if bufsize < 2:
+        for sample in data:
+            yield sample
+        return
+    initial = min(initial, bufsize)
+    buf = []
+    startup = True
+    for sample in data:
+        if len(buf) < bufsize:
+            buf.append(next(data))
+        k = random.randint(0, len(buf) - 1)
+        sample, buf[k] = buf[k], sample
+        if startup and len(buf) < initial:
+            buf.append(sample)
+            continue
+        startup = False
+        yield sample
+    for sample in buf:
+        yield sample
