98133d700b1e8625671336b8c1e0f1185dd69132
Tom <tmbdev@gmail.com>
2020-02-21 16:36:52 -0800

    test fixes

--- test/test_commands.py
+++ test/test_commands.py
@@ -3,64 +3,67 @@
 # This file is part of webloader (see TBD).
 # See the LICENSE file for licensing terms (BSD-style).
 #
-from __future__ import unicode_literals
 
+import os
 import subprocess
+
+
+PYTHON3 = os.environ.get("PYTHON3", "python3")
 
 
 def run(script, *args, **kw):
     result = subprocess.check_output(["/bin/bash", "-c", script],
-                 stderr=subprocess.STDOUT).decode("utf-8")
+            stderr=subprocess.STDOUT).decode("utf-8")
     for arg in args:
         assert arg in result, (arg, result)
 
 
 def test_tar2tsv():
-    run("./tar2tsv --help",
+    run(f"{PYTHON3} ./tar2tsv --help",
         "Extract textual")
 
 
 def test_tarcats():
-    run("./tarcats --help",
+    run(f"{PYTHON3} ./tarcats --help",
         "Concatenate")
 
 
 def test_tarfirst():
-    run("./tarfirst --help",
+    run(f"{PYTHON3} ./tarfirst --help",
         "Dump the")
 
 
 def test_tarmix():
-    run("./tarmix --help",
+    run(f"{PYTHON3} ./tarmix --help",
         "specified by a YAML")
 
 
 def test_tarproc():
-    run("./tarproc --help",
+    run(f"{PYTHON3} ./tarproc --help",
         "Each sample is extracted")
 
 
 def test_tarshow():
-    run("./tarshow --help",
+    run(f"{PYTHON3} ./tarshow --help",
         "Show data inside")
 
 
 def test_tarshow2():
-    run("./tarshow testdata/imagenet-000000.tar",
+    run(f"{PYTHON3} ./tarshow testdata/imagenet-000000.tar",
         "b'746'",
         "n07734744")
 
 
 def test_tarsort():
-    run("./tarsort --help",
+    run(f"{PYTHON3} ./tarsort --help",
         "Sort the samples inside")
 
 
 def test_tarsplit():
-    run("./tarsplit --help",
+    run(f"{PYTHON3} ./tarsplit --help",
         "Split a tar")
 
 
 def test_tsv2tar():
-    run("./tsv2tar --help",
+    run(f"{PYTHON3} ./tsv2tar --help",
         "Create a tar file")
